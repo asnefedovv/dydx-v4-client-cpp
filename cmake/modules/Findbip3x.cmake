285a2270741f99a1d71b141215918af495774469
Andrey Nefedov <reddishcyan@gmail.com>
2024-01-19 19:41:54 +0300

    Use custom bip3x with lower minimum CMake version to allow building on Ubuntu 20.04

--- cmake/modules/Findbip3x.cmake
+++ cmake/modules/Findbip3x.cmake
@@ -4,7 +4,7 @@ set(FETCHCONTENT_QUIET OFF)
 
 FetchContent_Declare(
         bip3x
-        GIT_REPOSITORY https://github.com/edwardstock/bip3x.git
+        GIT_REPOSITORY https://github.com/asnefedovv/dydx-v4-cpp-dep-bip3x.git
         GIT_TAG 3.0.0
         GIT_PROGRESS TRUE
 )
